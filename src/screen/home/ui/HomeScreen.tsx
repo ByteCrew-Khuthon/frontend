88122c2a431c69b956c6d1b66adb143d54129feb
cho4u4o <cho4u4o@gmail.com>
2025-05-09 21:05:04 +0900

    feat : implement `HomeScreen` with `AppBar`

--- src/screen/home/ui/HomeScreen.tsx
+++ src/screen/home/ui/HomeScreen.tsx
@@ -7,8 +7,7 @@ import { AppBar } from '@/shared/ui';
 export default function HomeScreen() {
   return (
     <div className='fixed inset-0 overflow-hidden'>
-      <AppScreen>
-        <AppBar />
+      <AppScreen appBar={AppBar()}>
         <div className='from-sub to-point absolute -z-1 h-[45%] w-full bg-gradient-to-b' />
         <div className='scrollbar-hide container-mobile gap-y-normal-padding p-normal-padding relative flex size-full flex-col overflow-scroll overflow-y-scroll'>
           <HomeContainer />
