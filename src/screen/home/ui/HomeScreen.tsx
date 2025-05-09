70032e9b36490c7fc34c0c58c41b30801181cb68
cho4u4o <cho4u4o@gmail.com>
2025-05-09 20:24:06 +0900

    feat : implement `HomeScreen`

--- src/screen/home/ui/HomeScreen.tsx
+++ src/screen/home/ui/HomeScreen.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React from 'react';
 
-import { AppScreen } from "@stackflow/plugin-basic-ui";
+import { AppScreen } from '@stackflow/plugin-basic-ui';
+import { HomeContainer } from '@/widgets/home/ui';
+import { AppBar } from '@/shared/ui';
 
 export default function HomeScreen() {
   return (
-    <div className="fixed inset-0 overflow-hidden">
+    <div className='fixed inset-0 overflow-hidden'>
       <AppScreen>
-        <div className="scrollbar-hide container-mobile gap-y-normal-spacing p-normal-padding pb-dock-height flex size-full flex-col overflow-scroll overflow-y-scroll"></div>
+        <AppBar />
+        <div className='from-sub to-point absolute -z-1 h-[48%] w-full bg-gradient-to-b' />
+        <div className='scrollbar-hide container-mobile gap-y-normal-padding p-normal-padding relative flex size-full flex-col overflow-scroll overflow-y-scroll'>
+          <HomeContainer />
+        </div>
       </AppScreen>
     </div>
   );
---
+++ src/widgets/home/ui/HomeContainer.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+import { FARM } from '@/mock';
+import { Card, CardItem } from '@/shared/ui';
+import { getDate } from '@/shared/utils';
+
+export default function HomeContainer() {
+  const today = new Date();
+  const data = Array.from({ length: 6 });
+
+  return (
+    <>
+      <div className='mt-12 flex items-center gap-2'>
+        <span className='border-point bg-point/30 w-fit rounded-full border-[1px] px-2 py-1 text-sm'>
+          농장
+        </span>
+        {FARM.location}
+      </div>
+      <div className='flex w-full justify-between'>
+        <div className='flex flex-col gap-1 text-2xl font-medium'>
+          <p>
+            <span className='font-bold'>유진</span>님, 좋은 아침이에요.
+          </p>
+          <p>
+            오늘의 <span className='font-bold'>{FARM.title}</span>을 확인하세요!
+          </p>
+        </div>
+        <button
+          name='farmStatus'
+          className='border-border bg-fill h-fit w-fit cursor-pointer rounded-full border-[1px] px-3 py-1.5 focus:outline-none'
+        >
+          더보기
+        </button>
+      </div>
+      <div className='flex flex-col gap-3'>
+        <CardItem className='h-30'>
+          <div className='flex size-full items-center justify-between'>
+            <div className='flex size-full items-center gap-4'>
+              <div className='rounded-small h-full w-24 bg-red-400' />
+              <div className='flex flex-col'>
+                <span className='text-lg font-medium'>지난밤 기침</span>
+                <p>
+                  <span className='text-xl font-bold'>0</span>회
+                </p>
+              </div>
+            </div>
+            <button className='rounded-small bg-point flex h-fit w-fit items-center justify-center px-4 py-2 text-nowrap text-white'>
+              자세히 보기
+            </button>
+          </div>
+        </CardItem>
+        <CardItem className='h-40'>
+          <div className='flex size-full items-center justify-between'>
+            <div className='flex h-full flex-col justify-between'>
+              <div className='flex flex-col'>
+                <span className='text-lg font-medium'>주변 농장의</span>
+                <span className='text-xl font-bold'>질병 발병 상황</span>
+              </div>
+              <span className='text-light'>
+                {getDate(today, 'YYYY.MM.DD 기준')}
+              </span>
+            </div>
+            <div
+              className='h-full w-40 bg-cover bg-center'
+              style={{
+                backgroundImage: `url(https://i.pinimg.com/736x/2c/82/ed/2c82edda3e2bce7f7e1ca55557790d97.jpg)`,
+              }}
+            />
+          </div>
+        </CardItem>
+      </div>
+      <div className='flex items-center justify-between'>
+        <span className='text-xl font-bold'>최근 농장 소식</span>
+        <button className='cursor-pointer focus:outline-none'>더보기</button>
+      </div>
+      <div className='scrollbar-hide h-card-height rounded-10 relative flex items-center gap-x-3 overflow-x-scroll'>
+        {data.map((_, i) => (
+          <Card key={i} className='h-50 w-40' />
+        ))}
+      </div>
+    </>
+  );
+}
