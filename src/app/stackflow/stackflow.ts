0abdbe32bb699abec02b49689982a3e980b380a5
cho4u4o <cho4u4o@gmail.com>
2025-05-09 21:03:43 +0900

    style : set background color

--- src/app/stackflow/stackflow.ts
+++ src/app/stackflow/stackflow.ts
@@ -1,18 +1,22 @@
-import { basicUIPlugin } from "@stackflow/plugin-basic-ui";
-import { basicRendererPlugin } from "@stackflow/plugin-renderer-basic";
-import { stackflow } from "@stackflow/react";
-import { HomeScreen } from "../../screen/home/ui";
+import { basicUIPlugin } from '@stackflow/plugin-basic-ui';
+import { basicRendererPlugin } from '@stackflow/plugin-renderer-basic';
+import { stackflow } from '@stackflow/react';
+
+import { FarmScreen } from '@/screen/farm/ui';
+import { HomeScreen } from '@/screen/home/ui';
 
 export const { Stack, useFlow } = stackflow({
   transitionDuration: 350,
   activities: {
     HomeScreen,
+    FarmScreen,
   },
   plugins: [
     basicRendererPlugin(),
     basicUIPlugin({
-      theme: "cupertino",
+      theme: 'cupertino',
+      backgroundColor: '#f4f4f4',
     }),
   ],
-  initialActivity: () => "HomeScreen",
+  initialActivity: () => 'HomeScreen',
 });
